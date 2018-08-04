80dd5e511b0ff9ef12dc6f0f7cd33205847b7d7a
Kyle Parrish <kfparri@gmail.com>
2018-08-04 08:32:55 -0500

    Updated the readme, and fixed the script so it works correctly on the Linux subsystem for Windows

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # elasticsearch-sample-dataset
 The elasticsearch sample datasets scripted to create local development with elasticsearch in docker
+
+## Setting up
+1. To setup, you must first have docker installed and running.  Once you have docker running, pull up your terminal and run:
+    ```
+    docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+    ```
+
+    You may also run it detached by adding the ```-d``` flag to the above command.
+
+2. Once the container is up and running, you can load the data by running the ```pushData.sh``` script 
+    - **Tip** if you pipe the script to an output file, it runs _much_ faster:
+    ``` 
+    ./pushData.sh > output.out 
+    ```
--- pushData.sh
+++ pushData.sh
@@ -1,4 +1,4 @@
-curl -X PUT "localhost:9200/shakespeare" -H 'Content-Type: application/json' -d'
+curl -X PUT "localhost:9200/shakespeare" -H 'Content-Type: application/json' -d '
 {
  "mappings": {
   "doc": {
@@ -10,10 +10,9 @@ curl -X PUT "localhost:9200/shakespeare" -H 'Content-Type: application/json' -d'
    }
   }
  }
-}
-'
+}'
 
-curl -X PUT "localhost:9200/logstash-2015.05.18" -H 'Content-Type: application/json' -d'
+curl -X PUT "localhost:9200/logstash-2015.05.18" -H 'Content-Type: application/json' -d '
 {
   "mappings": {
     "log": {
@@ -28,10 +27,9 @@ curl -X PUT "localhost:9200/logstash-2015.05.18" -H 'Content-Type: application/j
       }
     }
   }
-}
-'
+}'
 
-curl -X PUT "localhost:9200/logstash-2015.05.19" -H 'Content-Type: application/json' -d'
+curl -X PUT "localhost:9200/logstash-2015.05.19" -H 'Content-Type: application/json' -d '
 {
   "mappings": {
     "log": {
@@ -46,10 +44,9 @@ curl -X PUT "localhost:9200/logstash-2015.05.19" -H 'Content-Type: application/j
       }
     }
   }
-}
-'
+}'
 
-curl -X PUT "localhost:9200/logstash-2015.05.20" -H 'Content-Type: application/json' -d'
+curl -X PUT "localhost:9200/logstash-2015.05.20" -H 'Content-Type: application/json' -d '
 {
   "mappings": {
     "log": {
@@ -64,13 +61,9 @@ curl -X PUT "localhost:9200/logstash-2015.05.20" -H 'Content-Type: application/j
       }
     }
   }
-}
-'
+}'
 
 curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/bank/account/_bulk?pretty' --data-binary @accounts.json
 curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/shakespeare/doc/_bulk?pretty' --data-binary @shakespeare_6.0.json
 curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/_bulk?pretty' --data-binary @logs.jsonl
-
 curl -X GET "localhost:9200/_cat/indices?v"
-
-
